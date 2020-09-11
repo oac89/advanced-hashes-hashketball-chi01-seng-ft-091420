c455ade79b170e868b501a3257c628fce4d3c66f
Oscar A Castro <oacastro2@gmail.com>
2020-09-11 20:51:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -201,7 +201,7 @@ def player_stats(players_name)
   end
 end 
 
- def big_shoe_rebounds(big_foot_player)
+ def big_shoe_rebounds
    big_foot = 0 
  game_hash.each do |key, value|
    value[:players]
