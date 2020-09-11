c406f9c437f802d6ca056a919dcd4f20aa092a6c
Oscar A Castro <oacastro2@gmail.com>
2020-09-11 22:11:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -208,7 +208,7 @@ end
  game_hash.each do |key, value|
               #binding.pry
     value[:players].each do |player|  
-      player[:shoe] >  
+      player[:shoe]. 
       value[:shoe]
     binding.pry 
   end 
