5407c6a4ea69226634435919f7446abb5c84c9ef
Oscar A Castro <oacastro2@gmail.com>
2020-09-11 21:04:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -205,9 +205,8 @@ end
    big_foot = 0 
  game_hash.each do |key, value|
    value[:players].each do |big_foot_player|
-binding.pry 
-
-      if big_foot_player[:shoe] > big_foot 
+    if big_foot_player[:shoe] > big_foot 
+      binding.pry
         return player[:rebound] 
       end
     end
